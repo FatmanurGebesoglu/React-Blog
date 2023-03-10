60baf96a454daff8281f35b035f3f046142707ba
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-10 14:48:45 +0300

    Create Page edited

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,9 +1,29 @@
 import './blog.css';
-
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hook/useFetch';
 import React from 'react'
 
+
 export default function Blog() {
+  const {id} = useParams();
+  const url = 'http://localhost:5000/bloglar/'+id;
+
+  const {hata,yukleniyor, data:blog}= useFetch(url);
+
   return (
-    <div><h1>Blog</h1></div>
+    <div className='blog'>
+      {hata && <p className='error'>{hata}</p>}
+      {yukleniyor && <p className='loading'>Yükleniyor...</p>}
+      {blog && (
+        <>
+          <h2 className='page-title'>{blog.baslik}</h2>
+          <p className='time'>Okuma Süresi: {blog.okunmaSuresi}</p>
+          <ul>
+            {blog.kategoriler.map(kat=> <li key={kat}>{kat}</li>)}
+          </ul>
+          <p className='info'>{blog.icerik}</p>
+        </>
+      )}
+    </div>
   )
 }
